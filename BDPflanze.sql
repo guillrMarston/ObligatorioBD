6f6fc581e439d74153d5cab0e144961ab89985ab
Guillermo Recoba <87285996+guillrMarston@users.noreply.github.com>
2022-05-24 21:23:41 -0300

    Empezando juego de prueba

--- BDPflanze.sql
+++ BDPflanze.sql
@@ -1,10 +1,10 @@
-
 create database Pflanze
 use Pflanze
+GO
 
 create table Plantas(
 id_planta int identity not null,
-nombre_popular varchar(20) not null,
+nombre_popular varchar(40) not null,
 fecnac datetime not null,
 altura_cm decimal(10,2),
 fec_hora_medida datetime,
@@ -12,7 +12,7 @@ precio_usd decimal(10,2),
 
 constraint PK_Id_Planta primary key(id_planta),
 constraint CK_Fecha_Medida check ((altura_cm is null and fec_hora_medida is null) OR (altura_cm is not null and fec_hora_medida is not null)),
-constraint CK_Altura_Maxima check (altura_cm < 12),
+constraint CK_Altura_Maxima check (altura_cm < 12000),
 constraint CK_Fecha_Medida_Val check ((fec_hora_medida < getdate()) or fec_hora_medida is null),
 constraint CK_Fecha_Medida_Val_fecnac check ((fec_hora_medida > fecnac) or fec_hora_medida is null)
 )
@@ -63,3 +63,28 @@ create table ItemMantenimiento(
 	constraint FK_Producto foreign key (id_prod) references Productos(id_prod),
 	constraint FK_Mantenimeinto foreign key (id_mant) references MantenimientoPlantas(id_mantenimiento)
 )
+SELECT * FROM Plantas
+/*COMIENZO JUEGO DE PRUEBA*/
+INSERT INTO Plantas (nombre_popular,fecnac,
+            altura_cm,fec_hora_medida,precio_usd) 
+	 VALUES
+             ('Achilleas','20150523',
+             1500,'20160101',
+             4.00),
+             ('Agapanto Blanco',
+             '20140401',9600,
+             '20150409 10:34:09 AM',
+             7.52),
+             ('Agapanto Enano Blanco',
+             '20190129', NULL, NULL,
+             5.90),
+			 ('Alegría Nueva Guínea Blanca',
+			  '20201218',11500,'20210103 
+			  12:39:05 AM',2.15),
+			  ('Papus Cachus','20220522',
+			   NULL,NULL,25.25),
+			   ('Anturio schererianum','20220501',
+			    1245,'20220505 05:45:00 PM',20.50)
+
+
+/*FIN JUEGO DE PRUEBA*/
